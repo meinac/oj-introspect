e763f5c68788277fe903efb6ab9b4fec1f07aab0
Mehmet Emin INAC <mehmetemininac@gmail.com>
2022-10-31 20:48:40 +0100

    Implement byte tracking

--- ext/oj-introspect/introspect.c
+++ ext/oj-introspect/introspect.c
@@ -17,27 +17,68 @@ typedef struct _introspect_S {
 
   ByteOffsets *byte_offsets;
 
+  void (*delegated_free_func)(struct _ojParser *p);
   void (*delegated_open_object_func)(struct _ojParser *p);
   void (*delegated_open_object_key_func)(struct _ojParser *p);
   void (*delegated_close_object_func)(struct _ojParser *p);
 } *IntrospectDelegate;
 
-static void open_object_introspected(ojParser p) {
+static void dfree(ojParser p) {
   IntrospectDelegate d = (IntrospectDelegate)p->ctx;
 
+  xfree(d->byte_offsets->stack);
+  xfree(d->byte_offsets);
+  d->delegated_free_func(p);
+}
+
+static void ensure_byte_offsets_stack(IntrospectDelegate d) {
+    if (RB_UNLIKELY(d->byte_offsets->current == (d->byte_offsets->length - 1))) {
+        d->byte_offsets->length += BYTE_OFFSETS_STACK_INC_SIZE;
+        REALLOC_N(d->byte_offsets->stack, long, d->byte_offsets->length + BYTE_OFFSETS_STACK_INC_SIZE);
+    }
+}
+
+static long pop(ojParser p) {
+    IntrospectDelegate d = (IntrospectDelegate)p->ctx;
+
+    return d->byte_offsets->stack[--d->byte_offsets->current];
+}
+
+static void push(ojParser p) {
+    IntrospectDelegate d = (IntrospectDelegate)p->ctx;
+    ensure_byte_offsets_stack(d);
+
+    d->byte_offsets->stack[d->byte_offsets->current++] = p->cur;
+}
+
+static void open_object_introspected(ojParser p) {
+  push(p);
+
+  IntrospectDelegate d = (IntrospectDelegate)p->ctx;
   d->delegated_open_object_func(p);
 }
 
 static void open_object_key_introspected(ojParser p) {
-  IntrospectDelegate d = (IntrospectDelegate)p->ctx;
+  push(p);
 
+  IntrospectDelegate d = (IntrospectDelegate)p->ctx;
   d->delegated_open_object_key_func(p);
+}
+
+static void set_introspection_values(ojParser p) {
+    IntrospectDelegate d = (IntrospectDelegate)p->ctx;
+
+    volatile VALUE obj = rb_hash_new();
+    rb_hash_aset(obj, ID2SYM(rb_intern("start_byte")), INT2FIX(pop(p)));
+    rb_hash_aset(obj, ID2SYM(rb_intern("end_byte")), INT2FIX(p->cur));
+    rb_hash_aset(*(d->usual.vtail - 1), ID2SYM(rb_intern("__oj_introspection")), obj);
 }
 
 static void close_object_introspected(ojParser p) {
   IntrospectDelegate d = (IntrospectDelegate)p->ctx;
-
   d->delegated_close_object_func(p);
+
+  set_introspection_values(p);
 }
 
 static void init_introspect_parser(ojParser p) {
@@ -45,9 +86,10 @@ static void init_introspect_parser(ojParser p) {
 
   oj_init_usual(p, (Usual)d);
 
-  // p->ctx  = (void *)d; // point to the introspect delegate(Do we need this? It will be always the same address as usual is at the start of the IntrospectDelegate.)
+  // now function mangling...
+  d->delegated_free_func = p->free;
+  p->free = &dfree;
 
-  // // now function mangling...
   Funcs f = &p->funcs[TOP_FUN];
   d->delegated_open_object_func = f->open_object;
   d->delegated_close_object_func = f->close_object;
@@ -62,6 +104,12 @@ static void init_introspect_parser(ojParser p) {
   d->delegated_open_object_key_func = f->open_object;
   f->open_object  = open_object_key_introspected;
   f->close_object = close_object_introspected;
+
+  // Init stack
+  d->byte_offsets          = ALLOC(ByteOffsets);
+  d->byte_offsets->current = 0;
+  d->byte_offsets->stack   = ALLOC_N(long, BYTE_OFFSETS_STACK_INC_SIZE);
+  d->byte_offsets->length  = BYTE_OFFSETS_STACK_INC_SIZE;
 }
 
 VALUE oj_get_parser_introspect() {
