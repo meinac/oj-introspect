e25916e669bc12ae7056652093aff709dfbaa75e
Mehmet Emin INAC <mehmetemininac@gmail.com>
2022-11-01 17:33:15 +0100

    Add initial specs

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gemspec
 
 gem "benchmark-ips"
+gem "pry"
 gem "rake", "~> 12.0"
 gem "rspec", "~> 3.0"
 gem "rake-compiler"
--- spec/oj/introspect_spec.rb
+++ spec/oj/introspect_spec.rb
@@ -1,9 +1,14 @@
 RSpec.describe Oj::Introspect do
-  it "has a version number" do
-    expect(Oj::Introspect::VERSION).not_to be nil
-  end
+  describe "#parse" do
+    let(:parser) { described_class.new }
+    let(:test_json) { File.read("./spec/fixtures/test.json") }
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    subject(:parsed_data) { parser.parse(test_json) }
+
+    it "injects the byte offsets of each object" do
+      expect(parsed_data[:__oj_introspection]).to eq({ start_byte: 0, end_byte: 147 })
+      expect(parsed_data["top-level-array"][0][:__oj_introspection]).to eq({ start_byte: 57, end_byte: 141 })
+      expect(parsed_data["top-level-array"][0]["array-object"][:__oj_introspection]).to eq({ start_byte: 81, end_byte: 135 })
+    end
   end
 end
